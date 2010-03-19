363622070e52d5a15a2fb901bc41b132a0cb1cba
Mike Pountney <Mike.Pountney@gmail.com>
2010-03-19 15:48:09 +0000

    adding try/catch

--- src/ejb/client/oracle/ejb30/CartClient.java
+++ src/ejb/client/oracle/ejb30/CartClient.java
@@ -7,13 +7,18 @@ import javax.ejb.EJB;
 import javax.naming.InitialContext;
 
 public class CartClient {
-@EJB
-private static Cart cart;
+    @EJB
+    private static Cart cart;
+
     public static void main(String[] args) throws Exception {
         
-        System.out.println("Adding items to cart");
-        cart.addItem("Item1");
-        cart.addItem("Item2");
+        try { 
+            System.out.println("Adding items to cart");
+            cart.addItem("Item1");
+            cart.addItem("Item2");
+        } catch (Exception e) {
+            System.out.println("accessing cart FAILED: " + e);
+        }
         System.out.println("Listing cart contents");
         Collection items = cart.getItems();
         for (Iterator i = items.iterator(); i.hasNext();) {
